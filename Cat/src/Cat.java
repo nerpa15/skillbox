78c0302602bb37cebbf67f0ba0f04f7f19d4ed14
nerpa <nerpa15@yandex.ru>
2021-02-22 10:17:37 +0300

    Урок 3 Задание 2

--- Cat/src/Cat.java
+++ Cat/src/Cat.java
@@ -1,54 +1,58 @@
-public class Cat
-{
+public class Cat {
     private double originWeight;
     private double weight;
 
     private double minWeight;
     private double maxWeight;
+    private double weightFood;
 
 
-    public Cat()
-    {
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        weightFood = 0;
 
     }
 
-    public void meow()
-    {
+    public void meow() {
         weight = weight - 1;
         System.out.println("Meow");
     }
 
-    public void feed(Double amount)
-    {
+    public void feed(Double amount) {
         weight = weight + amount;
+        weightFood = weightFood + amount;
     }
 
-    public void drink(Double amount)
-    {
+    public void drink(Double amount) {
         weight = weight + amount;
+        weightFood = weightFood + amount;
     }
 
-    public Double getWeight()
-    {
+    public Double getWeight() {
         return weight;
     }
 
-    public String getStatus()
-    {
-        if(weight < minWeight) {
+    public Double getWeightFood() {
+        return weightFood;
+    }
+
+    public void pee() {
+        weight = weight - 0.1;
+        System.out.println("Что-то очень личное");
+
+    }
+
+    public String getStatus() {
+        if (weight < minWeight) {
             return "Dead";
-        }
-        else if(weight > maxWeight) {
+        } else if (weight > maxWeight) {
             return "Exploded";
-        }
-        else if(weight > originWeight) {
+        } else if (weight > originWeight) {
             return "Sleeping";
-        }
-        else {
+        } else {
             return "Playing";
         }
     }
--- Cat/src/Loader.java
+++ Cat/src/Loader.java
@@ -2,39 +2,10 @@ public class Loader {
     public static void main(String[] args) {
         Cat cat1 = new Cat();
         cat1.print();
-
-        Cat cat2 = new Cat();
-        cat2.print();
-
-        Cat cat3 = new Cat();
-        cat3.print();
-
-        Cat cat4 = new Cat();
-        cat4.print();
-
-        Cat cat5 = new Cat();
-        cat5.print();
-
-        System.out.println("Cat feed");
-        cat1.feed(0.5);
-        cat1.print();
-
-        System.out.println("Cat feed");
-        cat2.feed(1.0);
-        cat2.print();
-
-        System.out.println(cat3.getStatus());
-        while (cat3.getStatus() != "Exploded") {
-            cat3.feed(1000.0);
-        }
-        System.out.println(cat3.getStatus());
-        cat3.print();
-
-        System.out.println(cat4.getStatus());
-        while (cat4.getStatus() != "Dead") {
-            cat4.meow();
-        }
-        System.out.println(cat4.getStatus());
-        cat4.print();
+        cat1.feed(150.00 );
+        cat1.feed(100.00 );
+        cat1.pee();
+        cat1.pee();
+        System.out.println("Кошка съела "+cat1.getWeightFood());
     }
 }
